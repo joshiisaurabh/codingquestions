d030421e832ea5e04339358c8474b35e62fd462c
saurabh joshi <joshiisaurabh@gmail.com>
2024-04-04 10:24:13 +0530

    Update coding_questions_part3.js

--- coding_questions_part3.js
+++ coding_questions_part3.js
@@ -1,1 +1,12 @@
+1)Find non duplicates
+const arr = [5, 2, 2, 3, 5, 6, 3, 7];
+const nonDuplicates = [];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr.indexOf(arr[i]) === arr.lastIndexOf(arr[i])) {
+        nonDuplicates.push(arr[i]);
+    }
+}
+
+console.log(nonDuplicates); // Output: [6, 7]
 
